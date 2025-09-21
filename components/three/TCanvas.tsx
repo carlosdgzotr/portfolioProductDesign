e7f381833eeb27ff27b93be08962e82eb8ad3bd3
Carlos Dominguez <carlosdominguez@192.168.1.76>
2025-09-21 11:21:12 -0600

    Changes

---
+++ components/three/TCanvas.tsx
@@ -0,0 +1,44 @@
+import React, { Suspense } from 'react';
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import {
+  enFragmentShader,
+  enVertexShader,
+  jpFragmentShader,
+  jpVertexShader,
+} from '../../modules/shader';
+import { Background } from './Background';
+// import { Lense } from './Lense';
+// import { TextPlane } from './TextPlane';
+
+export const TCanvas = () => {
+  const OrthographicCamera = new THREE.OrthographicCamera(
+    -1,
+    1,
+    1,
+    -1,
+    -10,
+    10
+  );
+
+  return (
+    <Canvas camera={OrthographicCamera} dpr={window.devicePixelRatio}>
+      <Suspense fallback={null}>
+        <Background />
+        <Lense />
+        <TextPlane
+          text={['What shall', 'I create today?']}
+          vertexShader={enVertexShader}
+          fragmentShader={enFragmentShader}
+        />
+        <TextPlane
+          text={['今日は', '何を作ろうか?']}
+          vertexShader={jpVertexShader}
+          fragmentShader={jpFragmentShader}
+        />
+      </Suspense>
+      {/* helper */}
+      {/* <Stats /> */}
+    </Canvas>
+  );
+};
